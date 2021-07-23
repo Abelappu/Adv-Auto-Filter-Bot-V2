d47a0bc96ee856b3d49f3e5ffe8c4d3d52882405
Abelappu <87808209+Abelappu@users.noreply.github.com>
2021-07-23 22:29:12 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'OUR 🔥 GROUP', url="https://t.me/malayalam_cinema_group"
                                 )
                         ]
                     ]
